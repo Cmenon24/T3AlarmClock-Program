1070aef04846025e86fc669e01687180f722538d
Crystal <crystalgmenon@gmail.com>
2025-02-07 07:04:52 +0100

    Initial Commit

---
+++ clock.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+from alarm import Alarm  
+import time
+import os
+
+class Clock:
+    def __init__(self, alarm):
+        self.alarm = alarm
+
+    def check_alarm(self):
+        while True:
+            current_time = datetime.now()
+            if current_time.hour == self.alarm.hour and current_time.minute == self.alarm.minute:
+                print("Time to wake up!")
+                self.play_alarm()
+                break
+            else: 
+                print("Monitoring.....")
+                time.sleep(15) 
+
+    def play_alarm(self):
+        for _ in range(3):
+            os.system("afplay /System/Library/Sounds/Glass.aiff")  
---
+++ main.py
@@ -0,0 +1,18 @@
+from alarm import Alarm
+from clock import Clock 
+
+def main():
+    hour = int(input("Set alarm hour (24-hour format): "))
+    minute = int(input("Set alarm minute: "))
+    
+    alarm = Alarm(hour, minute)
+    
+    clock = Clock(alarm)
+    
+    print(f"Your alarm is set for {hour:02d}:{minute:02d}.")
+    
+    clock.check_alarm()
+
+if __name__ == "__main__":
+    main()
+
